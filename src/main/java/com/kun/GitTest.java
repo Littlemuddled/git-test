2b8fa37973ce35b343c77a48d934f72a1de350d7
kun <w404359020@163.com>
2022-08-11 16:47:40 +0800

    hello push commit

--- src/main/java/com/kun/GitTest.java
+++ src/main/java/com/kun/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
+        System.out.println("hello push");
     }
 
 }
