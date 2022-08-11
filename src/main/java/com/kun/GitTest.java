cc3b7671d5f2c14ea795b1f8fbc24b68faa50e32
kun <w404359020@163.com>
2022-08-11 16:11:42 +0800

    first commit

---
+++ src/main/java/com/kun/GitTest.java
@@ -0,0 +1,14 @@
+package com.kun;
+
+/**
+ * Desc:
+ * User:Administrator
+ * Date:2022-08-11 16:07
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
