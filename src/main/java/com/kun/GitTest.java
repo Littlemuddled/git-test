86e848cb6a30c4fb929795097f7c1472ab402bc2
Black <102404489+Littlemuddled@users.noreply.github.com>
2022-08-11 16:52:58 +0800

    pull commit

--- src/main/java/com/kun/GitTest.java
+++ src/main/java/com/kun/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 
 }
