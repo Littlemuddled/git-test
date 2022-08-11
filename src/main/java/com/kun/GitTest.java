478ecc150ced91eaefbe3d841ee40b6d25e93faa
kun <w404359020@163.com>
2022-08-11 17:30:41 +0800

    gitee commit

--- src/main/java/com/kun/GitTest.java
+++ src/main/java/com/kun/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello push");
         System.out.println("hello pull");
+        System.out.println("hello gitee");
     }
 
 }
